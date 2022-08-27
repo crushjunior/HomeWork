1e9f59993be8b31b8a305c86e6de2b2bfd5aad1b
crushjunior <94167663+crushjunior@users.noreply.github.com>
2022-08-27 12:28:58 +0300

    1 Task

---
+++ src/pro/sky/java/course1/homeWork_2408/Author.java
@@ -0,0 +1,19 @@
+package pro.sky.java.course1.homeWork_2408;
+
+public class Author {
+    final private String firstName;
+    final private String lastName;
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+}
---
+++ src/pro/sky/java/course1/homeWork_2408/Book.java
@@ -0,0 +1,29 @@
+package pro.sky.java.course1.homeWork_2408;
+
+public class Book {
+    final private String name;
+    final private Author author;
+    private int yearPublication;
+
+    public Book(String name, Author author, int yearPublication) {
+        this.name = name;
+        this.author = author;
+        this.yearPublication = yearPublication;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Author getAuthor() {
+        return this.author;
+    }
+
+    public int getYearPublication() {
+        return this.yearPublication;
+    }
+
+    public void setYearPublication(int yearPublication) {
+        this.yearPublication = yearPublication;
+    }
+}
